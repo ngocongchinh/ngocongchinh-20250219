57d4f739a88cd7f6ae0081504574b20c8b6e5ec0
chinh <ngo.chinh106@gmail.com>
2025-02-18 21:18:56 +0700

    feat: remove unuse code

--- backend/src/modules/crypto/crypto.service.ts
+++ backend/src/modules/crypto/crypto.service.ts
@@ -1,24 +1,11 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
 import { getQueryUrl } from 'src/utils/coingekco';
 import serialize from 'src/utils/serialize';
-// import trendingData from 'src/dummy/trending.json';
-// import globalData from 'src/dummy/global.json';
-// import marketData from 'src/dummy/market.json';
-// import gainerData from 'src/dummy/gainer.json';
-// import searchData from 'src/dummy/search.json';
-// import coinDetailData from 'src/dummy/coin_detail.json';
-// import coinPriceChartData from 'src/dummy/price_chart.json';
-// import coinPriceChartRangeData from 'src/dummy/price_chart_range.json';
-// import coinOhlcData from 'src/dummy/coin_ohlc.json';
-// import coinOhlcByRange from 'src/dummy/coin_ohlc_range.json';
 
 @Injectable()
 export class CryptoService {
-  private readonly apiUrl: string;
-
   constructor(private readonly httpService: HttpService) {}
 
   async searchCoins(query: string) {
@@ -26,7 +13,6 @@ export class CryptoService {
     try {
       const response = await firstValueFrom(this.httpService.get(url));
       return response.data;
-      // return searchData;
     } catch (error) {
       return error;
     }
@@ -38,7 +24,6 @@ export class CryptoService {
     try {
       const response = await firstValueFrom(this.httpService.get(url));
       return response.data;
-      // return marketData;
     } catch (error) {
       return error;
     }
@@ -50,7 +35,6 @@ export class CryptoService {
     try {
       const response = await firstValueFrom(this.httpService.get(url));
       return response.data;
-      // return gainerData;
     } catch (error) {
       return error;
     }
@@ -61,7 +45,6 @@ export class CryptoService {
     try {
       const response = await firstValueFrom(this.httpService.get(url));
       return response.data;
-      // return trendingData;
     } catch (error) {
       return error;
     }
@@ -72,7 +55,6 @@ export class CryptoService {
     try {
       const response = await firstValueFrom(this.httpService.get(url));
       return response.data;
-      // return globalData;
     } catch (error) {
       return error;
     }
@@ -83,7 +65,6 @@ export class CryptoService {
     try {
       const response = await firstValueFrom(this.httpService.get(url));
       return response.data;
-      // return coinDetailData;
     } catch (error) {
       return error;
     }
@@ -95,7 +76,6 @@ export class CryptoService {
     try {
       const response = await firstValueFrom(this.httpService.get(url));
       return response.data;
-      // return coinPriceChartData;
     } catch (error) {
       return error;
     }
@@ -107,7 +87,6 @@ export class CryptoService {
     try {
       const response = await firstValueFrom(this.httpService.get(url));
       return response.data;
-      // return coinOhlcData;
     } catch (error) {
       return error;
     }
