bffbc36e6ace031c1c0c36b5257454c3d333f7a2
chinh <ngo.chinh106@gmail.com>
2025-02-18 21:06:19 +0700

    feat: add theme config

--- frontend/src/Providers.tsx
+++ frontend/src/Providers.tsx
@@ -1,12 +1,16 @@
 import { Provider } from 'react-redux';
 import { Store } from '@reduxjs/toolkit';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
 
 const Providers: React.FC<React.PropsWithChildren<{ store: Store; pageProps: any; children: React.ReactNode }>> = ({
   children,
   store,
 }) => (
   <Provider store={store}>
-    <div>{children}</div>
+    <ThemeProvider theme={theme}>
+      <div>{children}</div>
+    </ThemeProvider>
   </Provider>
 );
 
